182613f35d4db441fae0ffefea44438a07ebd6f2
alexvicuna <vicunaalejandro@gmail.com>
2018-07-25 21:36:22 -0600

    - Website now hosted using firebase hosting - Took out request an appointment and file download for now

---
+++ public/assets/medicalhistorydownload.js
@@ -0,0 +1,34 @@
+var storage = firebase.storage();
+var httpsReference = storage.refFromURL('https://firebasestorage.googleapis.com/v0/b/emersonvicunadds-1.appspot.com/o/MedicalHistory.pdf?alt=media&token=08446965-f958-4735-852b-618ca443c91f');
+//var pathReference = storage.ref('MedicalHistory.pdf');
+
+httpsReference.getDownloadURL().then(function(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function(event) {
+        var blob = xhr.response;
+    };
+    xhr.open('GET', url);
+    xhr.send();
+}).catch(function(error) {
+
+    // A full list of error codes is available at
+    // https://firebase.google.com/docs/storage/web/handle-errors
+    switch (error.code) {
+        case 'storage/object_not_found':
+            // File doesn't exist
+            break;
+
+        case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+
+        case 'storage/canceled':
+            // User canceled the upload
+            break;
+
+        case 'storage/unknown':
+            // Unknown error occurred, inspect the server response
+            break;
+    }
+});
